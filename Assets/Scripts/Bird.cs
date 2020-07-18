656899b4c80543502bf8b3d62a2867c3aa7435ef
Avea <aveakrause@gmail.com>
2020-07-18 13:30:25 -0700

    added windmill and audio

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D body;
+    [SerializeField] private List<AudioClip> flapClips = new List<AudioClip>();
+    [SerializeField] private AudioSource audioSource;
     public float flapHeight;
     
     private void Start()
@@ -21,5 +23,6 @@ public class Bird : MonoBehaviour
     public void Flap()
     {
         body.velocity = Vector2.up * flapHeight;
+        audioSource.PlayOneShot(flapClips[UnityEngine.Random.Range(0, flapClips.Count)]);
     }
 }
