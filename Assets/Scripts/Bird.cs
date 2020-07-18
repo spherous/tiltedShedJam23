b40effb61ff9e7154b0cffba4992c230306b9da5
Avea <aveakrause@gmail.com>
2020-07-18 11:09:34 -0700

    Started adding player

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D body;
+    
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();    
+    }
+
+    public void Flap()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private Bird player;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            player.Flap();
+        }       
+    }
+}
